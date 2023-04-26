7312c55098ba67aa10e2e9e01f99447cc6651230
Jamorn Anantariyakul <admin@Jamorns-iMac.local>
2023-04-26 20:50:19 +0700

    fix: router

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-    <BrowserRouter>
+    <BrowserRouter basename='/ss-react'>
       <AuthProvider>
         <App />
       </AuthProvider>
